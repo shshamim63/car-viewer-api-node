764e0a0f12280d65f9a6e71c82d54b3ad9b70175
shshamim63 <shshamim63@gmail.com>
2023-04-07 23:30:37 +0600

    fix(model): created model for the users updated schema

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,6 +1,14 @@
 import { z } from 'zod'
-import { RegistrationBodySchema, UserResponseSchema, UserSchema } from '../schema/user/user.schema';
+import {
+    AuthenticatedUserSchema,
+    LoginBodySchema,
+    RegistrationBodySchema,
+    UserResponseSchema,
+    UserSchema,
+} from '../schema/user/user.schema'
 
-export type IUser = z.infer<typeof UserSchema>;
+export type IUser = z.infer<typeof UserSchema>
 export type IRegistrationBody = z.infer<typeof RegistrationBodySchema>
 export type IUserResponse = z.infer<typeof UserResponseSchema>
+export type ILoginBody = z.infer<typeof LoginBodySchema>
+export type IAuthenticatedUser = z.infer<typeof AuthenticatedUserSchema>
