9910dd41236008061719e024c6b90e3318a2a0fa
shshamim63 <shshamim63@gmail.com>
2023-04-07 23:34:09 +0600

    refactor(error): cleaned the error handler code to impore the readability

--- src/util/errorHandler.ts
+++ src/util/errorHandler.ts
@@ -1,14 +1,19 @@
-import { Request, Response, NextFunction} from "express"
+import { Request, Response, NextFunction } from 'express'
 
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if(err.description && err.statusCode === 400) {
-    res.send({
-      message: err.message,
-      description: err.description
-    }).status(err.statusCode)
-  } else {
-    res.send({
-      message: 'Unknown Error'
-    })
-  }
+export const errorHandler = (
+    err: any,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const errorResponse = {
+        message: 'Server Error',
+    }
+    let statusCode = 500
+
+    if (err.message) errorResponse['message'] = err.message
+    if (err.description) errorResponse['description'] = err.description
+    if (err.statusCode) statusCode = err.statusCode
+
+    res.send(errorResponse).status(statusCode)
 }
