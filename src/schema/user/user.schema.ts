f364118ff35f525d53e9ef8eda20ba01f1626b4f
shshamim63 <shshamim63@gmail.com>
2023-04-07 23:28:37 +0600

    feat(schema): modify user schema to allow response contain tokens

--- src/schema/user/user.schema.ts
+++ src/schema/user/user.schema.ts
@@ -3,23 +3,40 @@ import { z } from 'zod'
 const email = z.string().email()
 const username = z.string().min(6)
 
-export const RegistrationBodySchema = z.object({
+export const LoginBodySchema = z.object({
     email: email,
-    username: username,
     password: z.string().min(8),
-    confirmPassword: z.string().min(8),
-    avatar: z.string().optional()
-}).refine((val) => val.password === val.confirmPassword, {
-    message: 'Password and confirm password must match'
+})
+
+export const RegistrationBodySchema = LoginBodySchema.merge(
+    z.object({
+        username: username,
+        confirmPassword: z.string().min(8),
+        avatar: z.string().optional(),
+    })
+).refine((val) => val.password === val.confirmPassword, {
+    message: 'Password and confirm password must match',
 })
 
 export const UserResponseSchema = z.object({
+    avatar: z.string().optional(),
+    createdAt: z.date(),
     email: email,
+    _id: z.string(),
+    profileId: z.string().nullable().optional(),
+    updatedAt: z.date(),
     username: username,
-    profileId: z.string().optional(),
-    avatar: z.string().optional()
 })
 
-export const UserSchema = UserResponseSchema.merge(z.object({
-    passwordHash: z.string()
-}))
+export const UserSchema = UserResponseSchema.merge(
+    z.object({
+        passwordHash: z.string(),
+    })
+)
+
+export const AuthenticatedUserSchema = UserResponseSchema.merge(
+    z.object({
+        accessToken: z.string(),
+        refreshToken: z.string()
+    })
+)
