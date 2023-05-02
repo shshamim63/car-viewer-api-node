404b459a14291034a8b0e54ecf5d353c1a6d9dfc
shshamim63 <shshamim63@gmail.com>
2023-05-02 22:36:55 +0600

    docs(swagger): updated swagger document for activation route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,7 @@ import * as authController from '../controller/auth.controller'
 
 const router = Router()
 
-router.get('/activate/user', authController.activateUserAccount)
+router.post('/activate/user', authController.activateUserAccount)
 router.post('/user/login', authController.login)
 router.post('/user/registration', authController.registerUser)
 
