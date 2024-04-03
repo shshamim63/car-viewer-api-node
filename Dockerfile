dfab173e53db50af549099ae70c197606d57de8c
shshamim63 <shshamim63@gmail.com>
2024-04-03 23:30:36 +0600

    Setup grafana and prometheus

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY package.json ./
 RUN npm install --omit=dev
 COPY . .
 HEALTHCHECK --interval=35s --timeout=4s CMD curl -f https://localhost/ || exit 1
-EXPOSE 3000
+EXPOSE 8080
 USER myuser
 CMD [ "npm", "run", "dev" ]
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import client from 'prom-client'
+import responseTime from 'response-time'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
 
@@ -16,6 +18,20 @@ import { corsMiddleware } from './middlewares/cors.middleware'
 connectDB()
 
 const app = express()
+
+const collectDefaultMetrics = client.collectDefaultMetrics
+
+collectDefaultMetrics({
+    register: client.register,
+})
+
+const reqResTime = new client.Histogram({
+    name: 'http_express_req_time',
+    help: 'this tells how much time is taken by req and res',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [1, 50, 100, 200, 400, 500, 800, 1000, 2000],
+})
+
 const swaggerDocument = YAML.load('./swagger/staging.yaml')
 const options = {
     explorer: true,
@@ -29,6 +45,18 @@ app.use(
 app.use(bodyParser.json())
 
 app.use(corsMiddleware)
+
+app.use(
+    responseTime((req, res, time) => {
+        reqResTime
+            .labels({
+                method: req.method,
+                route: req.url,
+                status_code: res.statusCode,
+            })
+            .observe(time)
+    })
+)
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
 
