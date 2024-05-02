09bf2c2a9710384bab9dbfe9071ce4bee7a87c27
shshamim63 <shshamim63@gmail.com>
2024-05-02 17:54:01 +0600

    Added test for user has activated token url

--- test/Controller/auth/activation.test.ts
+++ test/Controller/auth/activation.test.ts
@@ -4,6 +4,8 @@ import { faker } from '@faker-js/faker'
 
 import { app } from '../../../src/app'
 import * as userDB from '../../../src/repositories/user.repository'
+import { mongodbUser } from '../../data/user.data'
+import { UserStatus } from '../../../src/interfaces/user.interface'
 
 describe('Tests for User Activation', () => {
     afterEach(() => {
@@ -11,6 +13,7 @@ describe('Tests for User Activation', () => {
     })
     const invalidToken = faker.string.hexadecimal({ length: 64 })
     const validToken = faker.string.hexadecimal({ length: 64 })
+    const alreadyActiveMessageContext = 'The user already has an active account'
     describe('Request validation', () => {
         test('Response should have 400 status code when token is misssing', async () => {
             const response = await request(app).post('/auth/user/activate')
@@ -39,6 +42,23 @@ describe('Tests for User Activation', () => {
                 body: { message },
             } = response
             expect(status).toEqual(401)
+            expect(typeof message).toBe('string')
+        })
+        test('Response should have 403 status code when decoded user has activated status', async () => {
+            const mongoUser = mongodbUser()
+            ;(jwt.verify as jest.Mock).mockReturnValue({
+                ...mongoUser,
+                status: UserStatus.Active,
+            })
+            const response = await request(app).post(
+                `/auth/user/activate?token=${validToken}`
+            )
+            const {
+                status,
+                body: { message },
+            } = response
+            expect(status).toEqual(403)
+            expect(message).toEqual(alreadyActiveMessageContext)
             expect(typeof message).toBe('string')
         })
     })
