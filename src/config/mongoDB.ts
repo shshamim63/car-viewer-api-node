a45be4810939fbbe4c0efa9eef4aedc009bfbcda
shshamim63 <shshamim63@gmail.com>
2024-05-07 16:29:33 +0600

    Added console to see the actual mongodb connection

--- src/config/mongoDB.ts
+++ src/config/mongoDB.ts
@@ -6,6 +6,7 @@ const connectionURL = mongoConfig.mongoURL
 
 const connectDB = async () => {
     try {
+        console.log('Mongo URl', connectionURL)
         const conn = await mongoose.connect(connectionURL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
